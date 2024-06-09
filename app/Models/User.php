1a0c3b14afc14b22eed9ef768eccbf19dbdc5e01
AchmadIzaaz <myachmadizaaz@gmail.com>
2024-06-09 21:58:50 +0700

    Fix last login casts datetime

--- app/Models/User.php
+++ app/Models/User.php
@@ -52,6 +52,7 @@ class User extends Authenticatable
         return [
             'email_verified_at' => 'datetime',
             'password' => 'hashed',
+            'last_login_at' => 'datetime',
         ];
     }
 
@@ -67,6 +68,12 @@ class User extends Authenticatable
                 'source' => 'username'
             ]
         ];
+    }
+
+
+    public function addtionalInformation()
+    {
+        return $this->hasOne(addtionalInformation::class);
     }
 
 }
