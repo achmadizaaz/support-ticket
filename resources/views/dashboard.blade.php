c6bc8a91bfabedf6bdc74c9050f4bb19cd69d736
AchmadIzaaz <myachmadizaaz@gmail.com>
2024-06-09 17:34:45 +0700

    Add start page dashboard

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -1,17 +1,27 @@
-<x-app-layout>
-    <x-slot name="header">
-        <h2 class="font-semibold text-xl text-gray-800 leading-tight">
-            {{ __('Dashboard') }}
-        </h2>
-    </x-slot>
+@extends('layouts.main')
 
-    <div class="py-12">
-        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
-            <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
-                <div class="p-6 text-gray-900">
-                    {{ __("You're logged in!") }}
-                </div>
+@section('title', 'Dashboard')
+    
+@section('content')
+<div class="container-fluid">
+    <!-- start page title -->
+    <div class="card p-3">
+        <div class="d-sm-flex align-items-center justify-content-between">
+            <h4 class="mb-sm-0 font-size-18">Dashboard</h4>
+            <div class="page-title-right">
+                <ol class="breadcrumb m-0">
+                    <li class="breadcrumb-item active">Dashboard</li>
+                </ol>
             </div>
         </div>
     </div>
-</x-app-layout>
+    <!-- end page title -->
+
+    <!-- start page main -->
+    <div class="card p-3">
+
+    </div>
+    <!-- end page main -->
+    
+</div> <!-- container-fluid -->
+@endsection
