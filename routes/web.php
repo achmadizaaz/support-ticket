b099b3c62fb81d735e74461c4a383d817d0977d3
AchmadIzaaz <myachmadizaaz@gmail.com>
2024-06-09 17:41:13 +0700

    Add user, role, permission routes

--- routes/web.php
+++ routes/web.php
@@ -1,23 +1,44 @@
 <?php
 
-use App\Http\Controllers\ProfileController;
+namespace App\Http\Controllers;
+
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
-});
-Route::get('/page', function () {
-    return view('page');
-});
+// Route utama
+Route::prefix('dashboard')->middleware('auth')->group(function () {
+    
+    // dashboard route
+    Route::get('/', function () { return view('dashboard');
+    })->name('dashboard');
 
-Route::get('/dashboard', function () {
-    return view('dashboard');
-})->middleware(['auth', 'verified'])->name('dashboard');
+    // user routes
+    Route::controller(UserController::class)->prefix('users')->group(function(){
+        Route::get('/', 'index')->name('users');
+        Route::get('{slug}/show', 'show')->name('users.show');
+        Route::get('create', 'create')->name('users.create');
+        Route::post('store', 'store')->name('users.store');
+        Route::get('{slug}/edit', 'edit')->name('users.edit');
+        Route::put('{slug}/update', 'update')->name('users.update');
+        Route::delete('{user:slug}/delete', 'destroy')->name('users.delete');
+        Route::post('change-password', 'changePassword')->name('users.change.password');
+    });
 
-Route::middleware('auth')->group(function () {
-    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
-    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
-    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+    // role routes
+    Route::controller(RoleController::class)->prefix('roles')->group(function(){
+        Route::get('/', 'index')->name('roles');
+        Route::get('{id}/show', 'show')->name('roles.show');
+        Route::get('create', 'create')->name('roles.create');
+        Route::post('store', 'store')->name('roles.store');
+        Route::delete('{id}/delete', 'destroy')->name('roles.delete');
+    });
+
+    // sync permission role route
+    Route::controller(SyncPermissionController::class)->prefix('sync-permissions')->group(function(){
+        Route::get('/', 'index')->name('sync.permissions');
+        Route::get('{role}/show', 'show')->name('sync.permissions.show');
+        Route::get('{role}', 'create')->name('sync.permissions.create');
+        Route::post('{role}', 'store')->name('sync.permissions.store');
+    });
 });
 
 require __DIR__.'/auth.php';
