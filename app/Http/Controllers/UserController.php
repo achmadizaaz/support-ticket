cb5bbe91b4d576f2f45b80c3e98a4ff2aad056da
AchmadIzaaz <myachmadizaaz@gmail.com>
2024-06-09 17:42:19 +0700

    Add users

---
+++ app/Http/Controllers/UserController.php
@@ -0,0 +1,132 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Requests\UserRequest;
+use App\Models\User;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Facades\Log;
+use Illuminate\Support\Facades\Storage;
+
+class UserController extends Controller
+{
+
+    protected $model;
+
+    private function __construct(User $user)
+    {
+        $this->model = $user;
+    }
+
+    public function index()
+    {
+        $users = $this->model->latest()->get();
+        return view('users.index', compact('users'));
+    }
+
+    public function create()
+    {
+        return view('users.create');
+    }
+
+    public function store(UserRequest $request)
+    {
+        try{
+            $image = NULL;
+            // Jika terdapat upload file image
+            if($request->image){
+                $filenameWithExt = $request->file('image')->getClientOriginalName();
+                $extension = $request->file('image')->getClientOriginalExtension();
+                $fileNameToStore = $filenameWithExt. '-'. time().'.'.$extension;
+                $image = $request->file('image')->storeAs('users/images', $fileNameToStore, 'public');
+            }
+
+            // Menambahkan data ke database
+            $this->model->create([
+                'image' => $image,
+                'username' => $request->username,
+                'name' => $request->name,
+                'email' => $request->email,
+                'password' => Hash::make($request->password),
+                'is_active' => $request->is_active,
+            ]);
+        }catch(\Exception $exception){
+            // Menyimpan log kegagalan sistem
+            Log::error($exception->getMessage());
+            return back()->withInput($request->all())->with('failed', 'Terjadi kesalahan sistem, silakan coba nanti');
+        }
+
+        return to_route('users')->with('success', 'User telah berhasil ditambahkan');
+    }
+
+    public function show($slug)
+    {
+        $this->model->where('slug', $slug)->first();
+        return view('users.show', compact('user'));
+    }
+    public function edit($slug)
+    {
+        $this->model->where('slug', $slug)->first();
+        return view('users.edit', compact('user'));
+    }
+
+    public function update(UserRequest $request, $slug)
+    {
+        try{
+            $user = $this->model->where('slug', $slug)->first();
+            $image = $user->image;
+            // Jika terdapat file upload image
+            // Update image 
+            if($request->image){
+                $filenameWithExt = $request->file('image')->getClientOriginalName();
+                $extension = $request->file('image')->getClientOriginalExtension();
+                $fileNameToStore = $filenameWithExt. '-'. time().'.'.$extension;
+                $image = $request->file('image')->storeAs('users/images', $fileNameToStore, 'public');
+
+                // Menghapus image user lama
+                // Jika user memiliki image sebelumnya
+                if($user->image){
+                    Storage::delete($user->image);
+                }
+            }
+            // Update data user
+            $user->update([ 
+                'image' => $image,
+                'name' => $request->name,
+                'password' => Hash::make($request->password),
+                'is_active' => $request->is_active,
+            ]);
+        }catch(\Exception $exception){
+            Log::error($exception->getMessage());
+            return back()->withInput($request->all())->with('failed', 'Terjadi kesalahan sistem, silakan coba nanti');
+        }
+        return to_route('users.show', $slug)->with('success', 'Data user berhasil diupdate!');
+    }
+
+    public function destroy($slug)
+    {
+        $user = $this->model->where('slug', $slug)->first();
+        $user->delete();
+
+        return to_route('users')->with('success',' User telah berhasil dihapus!');
+    }
+
+    public function changePassword(UserRequest $request, $slug)
+    {
+        try{
+            $user = $this->model->where('slug', $slug)->first();
+            // Update password user
+            $user->update([
+                'password' => Hash::make($request->password),
+            ]);
+        }catch(\Exception $exception){
+            Log::error($exception->getMessage());
+            return back()->with('failed', 'Terjadi kesalahan sistem, silakan coba nanti');
+        }
+
+        return back()->with('success', 'Katasandi berhasil diupdate!');
+    }
+
+    // End User Controller
+}
---
+++ app/Http/Requests/UserRequest.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Validation\Rule;
+
+class UserRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     */
+    public function authorize(): bool
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, \Illuminate\Contracts\Validation\ValidationRule|array<mixed>|string>
+     */
+    public function rules(): array
+    {
+        return [
+            'image' => 'mimes:png,jpg|max:2048',
+            'username' => 'required',
+            'email' => 'required|email',
+            'is_active' => 'required|boolean',
+            'password' => 'required|min:5'
+        ];
+    }
+}
