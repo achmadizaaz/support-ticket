ce4cafaf94448443da1087426216fadcb78c9a8c
achmadizaaz <myachmadizaaz@gmail.com>
2024-06-10 12:46:45 +0700

    Update seeder permission role level

--- database/seeders/PermissionSeeder.php
+++ database/seeders/PermissionSeeder.php
@@ -18,15 +18,15 @@ class PermissionSeeder extends Seeder
         // Reset cached roles and permissions
         app()[PermissionRegistrar::class]->forgetCachedPermissions();
 
-         // create permissions users
-         Permission::create(['name' => 'read users']);
-         Permission::create(['name' => 'create users']);
-         Permission::create(['name' => 'update users']);
-         Permission::create(['name' => 'delete users']);
+        // create permissions users
+        Permission::create(['name' => 'read users']);
+        Permission::create(['name' => 'create users']);
+        Permission::create(['name' => 'update users']);
+        Permission::create(['name' => 'delete users']);
 
         //  Create role Super Administrator
-         $role = Role::create(['name' => 'Super Administrator']);
-         // gets all permissions via Gate::before rule; see AuthServiceProvider
+        $role = Role::create(['name' => 'Super Administrator', 'level' => 10]);
+        // gets all permissions via Gate::before rule; see AuthServiceProvider
 
         // create demo users
         $user = \App\Models\User::factory()->create([
