d93f13f1d7b5c827e3f88416454be178635039a5
AchmadIzaaz <myachmadizaaz@gmail.com>
2024-07-02 20:56:36 +0700

    Fix photo user

--- resources/views/layouts/header.blade.php
+++ resources/views/layouts/header.blade.php
@@ -228,8 +228,8 @@
             <div class="dropdown d-inline-block">
                 <button type="button" class="btn header-item bg-light-subtle border-start border-end" id="page-header-user-dropdown"
                 data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                    <img class="rounded-circle header-profile-user" src="{{ asset('assets/images/users/avatar-1.jpg') }}"
-                        alt="Header Avatar">
+                    <img class="rounded-circle header-profile-user" src="@if(isset(Auth::user()->image)) {{ asset('storage/'.Auth::user()->image) }} @else {{ asset('assets/images/no-image.webp')  }} @endif"
+                        alt="{{ Auth::user()->name }}">
                     <span class="d-none d-xl-inline-block ms-1 fw-medium">{{ Auth::user()->name }}</span>
                     <i class="mdi mdi-chevron-down d-none d-xl-inline-block"></i>
                 </button>
