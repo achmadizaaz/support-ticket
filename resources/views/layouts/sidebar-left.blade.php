3c21d4e30cc0cba37b6fcfd2174dd6c6c5142460
AchmadIzaaz <myachmadizaaz@gmail.com>
2024-08-02 21:32:06 +0700

    Fix permission menu

--- resources/views/layouts/sidebar-left.blade.php
+++ resources/views/layouts/sidebar-left.blade.php
@@ -12,6 +12,7 @@
                         <span data-key="t-dashboard">Dashboard</span>
                     </a>
                 </li>
+                @can('read-users')
                 <li class="menu-title" data-key="t-components">Management User</li>
                 <li class="{{ request()->routeIs('users.*') ? 'mm-active' : '' }}">
                     <a href="{{ route('users') }}" class="{{ request()->routeIs('users.*') ? 'active' : '' }}">
@@ -19,30 +20,42 @@
                         <span data-key="t-users">Users</span>
                     </a>
                 </li>
+                @endcan
 
+                @canany(['read-roles', 'read-permissions', 'read-sync-permission-roles'])
                 <li>
                     <a href="javascript: void(0);" class="has-arrow">
                         <i data-feather="grid"></i>
                         <span data-key="t-rolepermission">Role Permission</span>
                     </a>
+                    @canany(['read-roles', 'read-permissions', 'read-sync-permission-roles'])
                     <ul class="sub-menu" aria-expanded="false">
+                        @can('read-roles')
                         <li>
                             <a href="{{ route('roles') }}">
                                 <span data-key="t-role">Role</span>
                             </a>
                         </li>
+                        @endcan
+                        @can('read-permissions')
                         <li>
                             <a href="{{ route('permissions') }}">
                                 <span data-key="t-role">Permission</span>
                             </a>
                         </li>
+                        @endcan
+                        @can('read-sync-permission-roles')
                         <li class="{{ request()->routeIs('sync.permissions.*') ? 'mm-active' : '' }}">
                             <a href="{{ route('sync.permissions') }}" class="{{ request()->routeIs('sync.permissions.*') ? 'active' : '' }}">
                                 <span data-key="t-permission">Sync Permission Role</span>
                             </a>
                         </li>
+                        @endcan
                     </ul>
+                    @endcanany
                 </li>
+                @endcanany
+                @can('read-options')
                 {{-- Option Menu --}}
                 <li class="menu-title" data-key="t-components">Options</li>
                 <li>
@@ -51,6 +64,7 @@
                         <span data-key="t-rolepermission">Settings</span>
                     </a>
                 </li>
+                @endcan
             </ul>
         </div>
     </div>
