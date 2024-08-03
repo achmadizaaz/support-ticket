d7115f380b5a985e291fac16facce9806a7bbfca
achmadizaaz <myachmadizaaz@gmail.com>
2024-08-03 12:55:44 +0700

    Fix reset password user

--- resources/views/auth/reset-password.blade.php
+++ resources/views/auth/reset-password.blade.php
@@ -1,39 +1,171 @@
-<x-guest-layout>
-    <form method="POST" action="{{ route('password.store') }}">
-        @csrf
+@extends('layouts.guest')
 
+@section('title', 'Reset Password')
+
+@push('head')
+<style>
+    html,
+body {
+  height: 100%;
+}
+
+.form-signin {
+  max-width: 720px;
+  padding: 1.2rem;
+}
+
+.form-signin .form-floating:focus-within {
+  z-index: 2;
+}
+
+.form-signin input[type="email"] {
+  margin-bottom: -1px;
+  border-bottom-right-radius: 0;
+  border-bottom-left-radius: 0;
+}
+
+.form-signin input[type="password"] {
+  margin-bottom: 10px;
+  border-top-left-radius: 0;
+  border-top-right-radius: 0;
+}
+
+    .bd-placeholder-img {
+      font-size: 1.125rem;
+      text-anchor: middle;
+      -webkit-user-select: none;
+      -moz-user-select: none;
+      user-select: none;
+    }
+
+    @media (min-width: 768px) {
+      .bd-placeholder-img-lg {
+        font-size: 3.5rem;
+      }
+    }
+
+    .b-example-divider {
+      width: 100%;
+      height: 3rem;
+      background-color: rgba(0, 0, 0, .1);
+      border: solid rgba(0, 0, 0, .15);
+      border-width: 1px 0;
+      box-shadow: inset 0 .5em 1.5em rgba(0, 0, 0, .1), inset 0 .125em .5em rgba(0, 0, 0, .15);
+    }
+
+    .b-example-vr {
+      flex-shrink: 0;
+      width: 1.5rem;
+      height: 100vh;
+    }
+
+    .bi {
+      vertical-align: -.125em;
+      fill: currentColor;
+    }
+
+    .nav-scroller {
+      position: relative;
+      z-index: 2;
+      height: 2.75rem;
+      overflow-y: hidden;
+    }
+
+    .nav-scroller .nav {
+      display: flex;
+      flex-wrap: nowrap;
+      padding-bottom: 1rem;
+      margin-top: -1px;
+      overflow-x: auto;
+      text-align: center;
+      white-space: nowrap;
+      -webkit-overflow-scrolling: touch;
+    }
+
+    .btn-bd-primary {
+      --bd-violet-bg: #712cf9;
+      --bd-violet-rgb: 112.520718, 44.062154, 249.437846;
+
+      --bs-btn-font-weight: 600;
+      --bs-btn-color: var(--bs-white);
+      --bs-btn-bg: var(--bd-violet-bg);
+      --bs-btn-border-color: var(--bd-violet-bg);
+      --bs-btn-hover-color: var(--bs-white);
+      --bs-btn-hover-bg: #6528e0;
+      --bs-btn-hover-border-color: #6528e0;
+      --bs-btn-focus-shadow-rgb: var(--bd-violet-rgb);
+      --bs-btn-active-color: var(--bs-btn-hover-color);
+      --bs-btn-active-bg: #5a23c8;
+      --bs-btn-active-border-color: #5a23c8;
+    }
+
+    .bd-mode-toggle {
+      z-index: 1500;
+    }
+
+    .bd-mode-toggle .dropdown-menu .active .bi {
+      display: block !important;
+    }
+  </style>
+@endpush
+
+@section('content')
+<main class="form-signin w-100 m-auto card rounded-4">
+    <form action="{{ route('password.store') }}" method="post">
+        @csrf
         <!-- Password Reset Token -->
         <input type="hidden" name="token" value="{{ $request->route('token') }}">
 
-        <!-- Email Address -->
-        <div>
-            <x-input-label for="email" :value="__('Email')" />
-            <x-text-input id="email" class="block mt-1 w-full" type="email" name="email" :value="old('email', $request->email)" required autofocus autocomplete="username" />
-            <x-input-error :messages="$errors->get('email')" class="mt-2" />
-        </div>
+        <div class="row">
+            <div class="d-flex justify-content-between align-items-center p-2 mb-4 border-bottom">
+                <div class="text-start col-8 align-middle">
+                    <p class="h5 fw-bold ">Reset Password</p>
+                </div>
+                <div class="text-center d-block auth-logo col-4">
+                    <img src="{{ asset('assets/images/laravel.png') }}" alt="{{ config('app.name', 'Laravel') }}" height="28"> <span class="logo-txt">{{ config('app.name', 'Laravel') }}</span>
+                </div>
+            </div>
+            @if($errors->any())
+                <div class="alert alert-danger" role="alert">
+                    <div class="d-flex justify-content-between border-bottom border-danger mb-2">
+                        <h5 class="alert-heading">Errors:</h5>
+                        <button type="button" class="btn-close small" data-bs-dismiss="alert" aria-label="Close" style="padding: 12px;"></button>
+                    </div>
+                    <ul>
+                        @foreach ($errors->all() as $error)
+                        <li>{{ $error }}</li>
+                        @endforeach
+                    </ul>
+                </div>
+            @endif
 
-        <!-- Password -->
-        <div class="mt-4">
-            <x-input-label for="password" :value="__('Password')" />
-            <x-text-input id="password" class="block mt-1 w-full" type="password" name="password" required autocomplete="new-password" />
-            <x-input-error :messages="$errors->get('password')" class="mt-2" />
-        </div>
+            <!-- Email Address -->
+            <div class="mb-3">
+                <label for="email" class="form-label">Email<span class="text-danger fst-italic">*</span></label>
+                <input type="email" class="form-control" name="email" required placeholder="Enter your email account" autofocus value="{{ old('email', $request->email) }}" id="email">
+            </div>
+            
+            <!-- Password -->
+            <div class="mb-3">
+                <label for="password" class="form-label">Password<span class="text-danger fst-italic">*</span></label>
+                <input type="password" class="form-control" name="password" required placeholder="Enter new password" autofocus value="{{ old('password', $request->password) }}" autocomplete="new-password" id="password">
+            </div>
+            
+            <!-- Confirm Password -->
+            <div class="mb-3">
+                <label for="password_confirmation" class="form-label">Confirm Password<span class="text-danger fst-italic">*</span></label>
+                <input type="password" class="form-control" name="password_confirmation" required placeholder="Enter confirm password" autofocus value="{{ old('password', $request->password) }}" autocomplete="new-password" id="password_confirmation">
+            </div>
 
-        <!-- Confirm Password -->
-        <div class="mt-4">
-            <x-input-label for="password_confirmation" :value="__('Confirm Password')" />
+            <!-- Button Reset Password -->
+            <div class="mb-3">
+                <button class="btn btn-primary waves-effect waves-light" type="submit">
+                    <i class="bi bi-link-45deg"></i> Reset Password
+                </button>
+            </div>
 
-            <x-text-input id="password_confirmation" class="block mt-1 w-full"
-                                type="password"
-                                name="password_confirmation" required autocomplete="new-password" />
-
-            <x-input-error :messages="$errors->get('password_confirmation')" class="mt-2" />
-        </div>
-
-        <div class="flex items-center justify-end mt-4">
-            <x-primary-button>
-                {{ __('Reset Password') }}
-            </x-primary-button>
         </div>
     </form>
-</x-guest-layout>
+</main>
+
+@endsection
