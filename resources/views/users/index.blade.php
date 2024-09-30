75eaea6698845491a423306533bf3d08b995cf81
achmadizaaz <myachmadizaaz@gmail.com>
2024-09-30 09:23:28 +0700

    Fix permission users

--- resources/views/users/index.blade.php
+++ resources/views/users/index.blade.php
@@ -181,24 +181,33 @@
                             {{-- <td class="align-middle">{!! $user->last_login_at ? $user->last_login_at->diffForHumans() : '<span class="fst-italic">Belum pernah login</span>'  !!}</td> --}}
                             <td class="align-middle">
                                 <div class="d-flex gap-1">
-                                    {{-- Detail Button --}}
-                                    <a href="{{ route('users.show', $user->slug) }}" class="btn btn-sm btn-info" title="Show detail user">
-                                        <i class="bi bi-eye"></i>
-                                    </a>
-                                    {{-- Edit Button --}}
-                                    <a href="{{ route('users.edit', $user->slug) }}" class="btn btn-sm btn-warning" title="Edit user">
-                                        <i class="bi bi-pencil-square"></i>
-                                    </a>
-                                    {{-- Change password button --}}
-                                    <button type="button" class="btn btn-sm btn-success changePasswordUser" data-bs-toggle="modal" data-bs-target="#changePasswordUserModal" data-username="{{ $user->username }}" data-id="{{ $user->id }}" title="Change password user">
-                                        <i class="bi bi-shield-lock"></i>
-                                    </button>
+                                    @can('read-users')
+                                        {{-- Detail Button --}}
+                                        <a href="{{ route('users.show', $user->slug) }}" class="btn btn-sm btn-info" title="Show detail user">
+                                            <i class="bi bi-eye"></i>
+                                        </a>
+                                    @endcan
 
-                                    {{-- Delete button --}}
-                                    <button type="button" class="btn btn-sm btn-danger confirm_delete" data-bs-toggle="modal" data-bs-target="#deleteModal" data-username="{{ $user->username }}" data-id="{{ $user->id }}" title="Delete user">
-                                        <i class="bi bi-trash3"></i>
-                                    </button>
+                                    @can('update-users')
+                                        {{-- Edit Button --}}
+                                        <a href="{{ route('users.edit', $user->slug) }}" class="btn btn-sm btn-warning" title="Edit user">
+                                            <i class="bi bi-pencil-square"></i>
+                                        </a>
+                                    @endcan
 
+                                    @can('change-password-users')
+                                        {{-- Change password button --}}
+                                        <button type="button" class="btn btn-sm btn-success changePasswordUser" data-bs-toggle="modal" data-bs-target="#changePasswordUserModal" data-username="{{ $user->username }}" data-id="{{ $user->id }}" title="Change password user">
+                                            <i class="bi bi-shield-lock"></i>
+                                        </button>
+                                    @endcan
+                                    
+                                    @can('delete-users')
+                                        {{-- Delete button --}}
+                                        <button type="button" class="btn btn-sm btn-danger confirm_delete" data-bs-toggle="modal" data-bs-target="#deleteModal" data-username="{{ $user->username }}" data-id="{{ $user->id }}" title="Delete user">
+                                            <i class="bi bi-trash3"></i>
+                                        </button>
+                                    @endcan
                                 </div>
                             </td>
                         </tr>
@@ -217,93 +226,102 @@
         <!-- end page main -->
     </div>
 
-    <!-- Modal Change password user -->
-    <div class="modal fade" id="changePasswordUserModal" tabindex="-1" aria-labelledby="changePasswordUserModalLabel" aria-hidden="true">
-        <div class="modal-dialog">
-            <div class="modal-content">
-                <div class="modal-header">
-                <h1 class="modal-title fs-5" id="changePasswordUserModalLabel">Change Password: 
-                    <span class="text-success changePasswordUserName"></span>
-                </h1>
-                <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
-                </div>
-                <form action="#" id="formChangePassword" method="POST">
-                    <div class="modal-body">
-                        @csrf
-                        @method('PUT')
-                        <div class="mb-2">
-                            Silakan masukan katasandi baru untuk pengguna: <span class="changePasswordUserName"></span>
+    @can('change-password-users')
+        <!-- Modal Change password user -->
+        <div class="modal fade" id="changePasswordUserModal" tabindex="-1" aria-labelledby="changePasswordUserModalLabel" aria-hidden="true">
+            <div class="modal-dialog">
+                <div class="modal-content">
+                    <div class="modal-header">
+                    <h1 class="modal-title fs-5" id="changePasswordUserModalLabel">Change Password: 
+                        <span class="text-success changePasswordUserName"></span>
+                    </h1>
+                    <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                    </div>
+                    <form action="#" id="formChangePassword" method="POST">
+                        <div class="modal-body">
+                            @csrf
+                            @method('PUT')
+                            <div class="mb-2">
+                                Silakan masukan katasandi baru untuk pengguna: <span class="changePasswordUserName"></span>
+                            </div>
+                            <label for="changePasswordInput" class="form-label">
+                                New Password
+                            </label>
+                            <input type="text" class="form-control" name="change_password" id="changePasswordInput" placeholder="Enter a new password" required>
                         </div>
-                        <label for="changePasswordInput" class="form-label">
-                            New Password
-                        </label>
-                        <input type="text" class="form-control" name="change_password" id="changePasswordInput" placeholder="Enter a new password" required>
-                    </div>
-                    <div class="modal-footer">
-                        <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Tutup</button>
-                        <button type="submit" class="btn btn-success">Change Password.</button>
-                    </div>
-                </form>
-            </div>
-        </div>
-    </div>
-
-    <!-- Modal Delete user -->
-    <div class="modal fade" id="deleteModal" tabindex="-1" aria-labelledby="deleteModalLabel" aria-hidden="true">
-        <div class="modal-dialog">
-            <div class="modal-content">
-                <div class="modal-header">
-                <h1 class="modal-title fs-5" id="deleteModalLabel">Delete User</h1>
-                <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                        <div class="modal-footer">
+                            <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Tutup</button>
+                            <button type="submit" class="btn btn-success">Change Password.</button>
+                        </div>
+                    </form>
                 </div>
-                <form action="#" id="formDelete" method="POST">
-                    <div class="modal-body">
-                        @csrf
-                        @method('DELETE')
-                        <label for="confirm_delete" class="form-label">
-                            Untuk melanjutkan penghapusan pengguna, silakan ketik: <span class="text-danger" id="modalUsername"></span>
-                        </label>
-                        <input type="text" class="form-control" name="confirm" id="confirm_delete" required>
-                    </div>
-                    <div class="modal-footer">
-                        <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Tutup</button>
-                        <button type="submit" class="btn btn-danger">Delete it.</button>
-                    </div>
-                </form>
             </div>
         </div>
-    </div>
+    @endcan
+
+    @can('delete-users')
+        <!-- Modal Delete user -->
+        <div class="modal fade" id="deleteModal" tabindex="-1" aria-labelledby="deleteModalLabel" aria-hidden="true">
+            <div class="modal-dialog">
+                <div class="modal-content">
+                    <div class="modal-header">
+                    <h1 class="modal-title fs-5" id="deleteModalLabel">Delete User</h1>
+                    <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                    </div>
+                    <form action="#" id="formDelete" method="POST">
+                        <div class="modal-body">
+                            @csrf
+                            @method('DELETE')
+                            <label for="confirm_delete" class="form-label">
+                                Untuk melanjutkan penghapusan pengguna, silakan ketik: <span class="text-danger" id="modalUsername"></span>
+                            </label>
+                            <input type="text" class="form-control" name="confirm" id="confirm_delete" required>
+                        </div>
+                        <div class="modal-footer">
+                            <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Tutup</button>
+                            <button type="submit" class="btn btn-danger">Delete it.</button>
+                        </div>
+                    </form>
+                </div>
+            </div>
+        </div>
+    @endcan
 @endsection
 
 @push('scripts')
     <script>
-        // Change password
-        $('.changePasswordUser').click(function(e) {
-            let username = $(this).data('username');
-            let id = $(this).data('id');
-        
-            $('.changePasswordUserName').html(username);
+        @can('change-password-users')
+            // Change password
+            $('.changePasswordUser').click(function(e) {
+                let username = $(this).data('username');
+                let id = $(this).data('id');
             
-            // Route delete user
-            let url = "{{ route('users.change.password', ':id') }}";
-            route = url.replace(':id', id);
-            // Action route for delete user
-            $('#formChangePassword').attr('action', route);
-        });
+                $('.changePasswordUserName').html(username);
+                
+                // Route delete user
+                let url = "{{ route('users.change.password', ':id') }}";
+                route = url.replace(':id', id);
+                // Action route for delete user
+                $('#formChangePassword').attr('action', route);
+            });
+        @endcan
 
-        // Delete user
-        $('.confirm_delete').click(function(e) {
-            let username = $(this).data('username');
-            let id = $(this).data('id');
-        
-            $('#modalUsername').html(username);
-            $('#confirm_delete').attr('placeholder', 'Ketikan: "'+username+'"')
+        @can('delete-users')
+            // Delete user
+            $('.confirm_delete').click(function(e) {
+                let username = $(this).data('username');
+                let id = $(this).data('id');
             
-            // Route delete user
-            let url = "{{ route('users.delete', ':id') }}";
-            route = url.replace(':id', id);
-            // Action route for delete user
-            $('#formDelete').attr('action', route);
-        });
+                $('#modalUsername').html(username);
+                $('#confirm_delete').attr('placeholder', 'Ketikan: "'+username+'"')
+                
+                // Route delete user
+                let url = "{{ route('users.delete', ':id') }}";
+                route = url.replace(':id', id);
+                // Action route for delete user
+                $('#formDelete').attr('action', route);
+            });
+        @endcan
+
     </script>
 @endpush
